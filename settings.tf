cbc96fbfab36ba458779f152ffa6110ca82c6914
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2020-12-15 14:43:02 -0500

    version constraints

--- settings.tf
+++ settings.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.14.1"
+  required_version = "~> 0.14"
 
   required_providers {
     github = {
-      source = "hashicorp/github"
-      version = ">= 4.1.0"
+      source  = "hashicorp/github"
+      version = "~> 4.1.0"
     }
   }
 }
